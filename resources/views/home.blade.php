776e36bec8eebb71c0c41057803e45d965c6a300
francesco <cesaranofrancescomain@gmail.com>
2023-01-30 16:26:15 +0100

    add card * 10

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,89 @@
 <div class="body-container">
     <div class="wrapper">
         <button class="button absolute"> text </button>
-        {{-- <ul>
-            <Card v-for="(element, index) in myJson" :key="index" :thumb="element.thumb" :series="element.series" />
-        </ul> --}}
+        <ul>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            <li>
+                <div class="wrapper">
+                    <div class="img-container">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+                    </div>
+                    <h5> series </h5>
+                </div>
+            </li>
+            
+        </ul>
         <div class="button-container">
             <button class="button second "> text </button>
         </div>
