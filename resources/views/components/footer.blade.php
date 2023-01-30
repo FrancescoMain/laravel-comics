990e621f785d0c5786e6f95842f1c8ad692ccb7c
francesco <cesaranofrancescomain@gmail.com>
2023-01-30 16:59:56 +0100

    created footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,122 @@
+ <footer>
+        <div class="container-bot">
+            <div class="sizing">
+                <div class="box">
+                    <ul>
+                        <h3>section.title </h3>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h3>section.title </h3>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <ul>
+                        <h3>dc.title</h3>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li>
+                            <a href="element.link">element.title </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <ul>
+                        <h3>sites.title </h3>
+                        <li >
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li >
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li >
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li >
+                            <a href="element.link">element.title </a>
+                        </li>
+                        <li >
+                            <a href="element.link">element.title </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="logo-big">
+                    <img src="{{Vite::asset('public/img/dc-logo-bg.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="bottom-container">
+            <div class="bottom">
+                <div class="button-bot">
+                    <h5>SING - UP NOW</h5>
+                </div>
+                <div class="social">
+                    <h3>FOLLOW US</h3>
+                    <img src="{{Vite::asset('public/img/footer-youtube.png')}}" alt="">
+                    <img src="{{Vite::asset('public/img/footer-youtube.png')}}" alt="">
+                    <img src="{{Vite::asset('public/img/footer-youtube.png')}}" alt="">
+                    <img src="{{Vite::asset('public/img/footer-youtube.png')}}" alt="">
+                    <img src="{{Vite::asset('public/img/footer-youtube.png')}}" alt="">
+                </div>
+            </div>
+
+        </div>
+
+    </footer>
