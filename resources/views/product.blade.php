38f9909c332016b73e7716312266603d3c91506d
francesco <cesaranofrancescomain@gmail.com>
2023-01-30 17:38:06 +0100

    created /product

---
+++ resources/views/product.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.base')
+
+@section('content')
+    <div class="myContainer bg-white">
+    <ul>
+        <li>
+            <img src="{{Vite::asset('public/img/buy-comics-subscriptions.png')}}" alt="">
+            <a class="black" href="">link.title</a>
+        </li>
+        <li>
+            <img src="{{Vite::asset('public/img/buy-comics-subscriptions.png')}}" alt="">
+            <a class="black" href="">link.title</a>
+        </li>
+        <li>
+            <img src="{{Vite::asset('public/img/buy-comics-subscriptions.png')}}" alt="">
+            <a class="black" href="">link.title</a>
+        </li>
+        <li>
+            <img src="{{Vite::asset('public/img/buy-comics-subscriptions.png')}}" alt="">
+            <a class="black" href="">link.title</a>
+        </li>
+        <li>
+            <img src="{{Vite::asset('public/img/buy-comics-subscriptions.png')}}" alt="">
+            <a class="black" href="">link.title</a>
+        </li>
+        
+    </ul>
+</div>
+@endsection
