973e2b11e7bca2c3aa7b63bebd87165977a2bf09
francesco <cesaranofrancescomain@gmail.com>
2023-01-30 16:15:38 +0100

    added buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,19 @@
 @extends('layout.base')
 
 @section('content')
+<div class="body-container">
+    <div class="wrapper">
+        <button class="button absolute"> text </button>
+        {{-- <ul>
+            <Card v-for="(element, index) in myJson" :key="index" :thumb="element.thumb" :series="element.series" />
+        </ul> --}}
+        <div class="button-container">
+            <button class="button second "> text </button>
+        </div>
+
+    </div>
+
+</div>
 
 
 @endsection
