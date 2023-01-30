f0fe1810970a30ebdbad6db44a26e3369b25d81a
francesco <cesaranofrancescomain@gmail.com>
2023-01-30 15:33:46 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.base')
+
+@section('content')
+<header>
+        <div class="container">
+            <div class="logo">
+                <img src="getImageUrl('../../public/img/dc-logo.png')" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+@endsection
