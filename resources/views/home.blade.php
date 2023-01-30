17378688567f72c038984e6921ee21a77b67c698
francesco <cesaranofrancescomain@gmail.com>
2023-01-30 16:03:14 +0100

    deleted header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,6 @@
 @extends('layout.base')
 
 @section('content')
-<header>
-        <div class="container">
-            <div class="logo">
-                <img src="getImageUrl('../../public/img/dc-logo.png')" alt="">
-            </div>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="link.link"> link.title</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+
 
 @endsection
