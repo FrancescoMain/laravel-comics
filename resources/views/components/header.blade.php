a9163cf4aa81f58fe1dfc319c701fef057a7b9e3
francesco <cesaranofrancescomain@gmail.com>
2023-01-30 16:03:26 +0100

    create header like component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,41 @@
+<header>
+        <div class="container">
+            <div class="logo">
+                <img src="{{Vite::asset('public/img/dc-logo.png')}}" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>
+                    <li>
+                        <a href="link.link"> link.title</a>
+                    </li>                    
+                </ul>
+            </nav>
+        </div>
+    </header>
